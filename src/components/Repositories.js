2a6660790530881e05c86a9d3dbe0f172dd724ce
Pedro Cisternas <pedrocisternasd@gmail.com>
2017-12-26 14:50:01 -0300

    Manejo de error básico

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -6,7 +6,8 @@ class Repositories extends Component {
 
         this.state = {
             userName: '',
-            repositories: props.repositories
+            repositories: props.repositories,
+            error: {error:{message:''}}
         };
         this.handleChange = this.handleChange.bind(this);
     }
@@ -20,16 +21,40 @@ class Repositories extends Component {
         fetch(`https://api.bitbucket.org/2.0/repositories/${userName}?pagelen=100`).then((response) => {
             return response.json();
         }).then(data =>{
-            self.setState({repositories: data.values});
+            if(data.type === 'error'){
+                self.setState({error: data});
+                self.state.error = data;
+                return false;
+            }
+            //self.setState({repositories: [...self.setState, data.values]});
+        }).catch((error) => {
+
         });
     }
 
+    handleErrors(response) {
+        if (!response.ok) throw Error(response.statusText);
+        return response;
+    }
+
     render() {
+        let error = null;
+
+        const repositoriesElement = this.state.repositories.map(function(repository){
+            return (<li>{repository}</li>)
+        });
+
+        if(this.state.error)  {
+            error = <h1>{this.state.error.error}</h1>
+        }
         return (
             <div>
                 <input className="form-control" type="text" placeholder="Type the name of your repository" value={this.state.userName} onChange={this.handleChange} />
                 <button className="btn btn-outline-primary" onClick={(e) => this.getRepositories(this.state.userName, e)}>Primary</button>
-
+                <ul>
+                    {repositoriesElement}
+                </ul>
+                <h1>{this.state.error.error.message}</h1>
             </div>
         );
     }
